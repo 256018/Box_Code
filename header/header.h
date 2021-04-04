56c667ba249eeaeafe3cf9c0fc11f77c7c1758b6
rohaninit <rohangupta.1106@gmail.com>
2021-04-05 00:46:50 +0530

    Updated

--- header/header.h
+++ header/header.h
@@ -2,6 +2,11 @@
 #define __BOX_C__
 #include<stdio.h>
 #include<stdlib.h>
+
+/**
+ * @brief define different color of boxes.
+ * 
+ */
 typedef enum color_t
 {
     RED = 0xFF, /**< Colour Code for RED */
@@ -9,6 +14,10 @@ typedef enum color_t
     GREEN       /**< Colour Code for GREEN */
 } color_t;
 
+/**
+ * @brief Define structure of a box.
+ * 
+ */
 typedef struct box
 {
     int id;
@@ -20,6 +29,10 @@ typedef struct box
     struct box *next;
 }box;
 
+/**
+ * @brief Define different type of errors.
+ * 
+ */
 typedef enum error_t
 {
     ERROR_NULL_PTR = -1,    /**< Null pointer dereferncing error */
@@ -36,8 +49,35 @@ float avg_vol;
 int no_of_box_by_color;
 float diff_btw_maxvol_minvol;
 
+/**
+ * @brief Create a box list object
+ * 
+ * @param head_ref 
+ * @param new_id 
+ * @param new_length 
+ * @param new_breadth 
+ * @param new_height 
+ * @param new_weight 
+ * @param new_color 
+ * @return error_t 
+ */
 error_t create_box_list(box** head_ref, int new_id, float new_length, float new_breadth, float new_height, float new_weight, color_t new_color);
+
+/**
+ * @brief Defination of print_box function.
+ * 
+ * @param head_ref 
+ * @return error_t 
+ */
 error_t print_box(box *head_ref);
+
+/**
+ * @brief Defination of search_box function
+ * 
+ * @param head_ref 
+ * @param id 
+ * @return error_t 
+ */
 error_t search_box(box *head_ref,int id);
 error_t count_box_color(color_t color, box *head_ref);
 error_t average_volume_boxs(box *head_ref);
